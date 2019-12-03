9fab8b33d36f41db20f43b061d91b0a69d15aad3
PSI <tkddls8848@naver.com>
2019-12-03 17:08:20 +0900

    알고리즘 수정

--- synapTest/src/main/java/com/synap/service/TestService.java
+++ synapTest/src/main/java/com/synap/service/TestService.java
@@ -1,129 +1,93 @@
 package com.synap.service;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 import java.util.Stack;
 
 public class TestService {
 
-	// 개별 번호 읽기
-	public static String readNumber(char ch, int unit) {
-		switch (ch) {
-		case '0':
-			return "";
-		// 만 이상에만 숫자를 부름
-		case '1':
-			if (unit == 0) {
-				return "일";
-			} else {
-				return "";
-			}
-		case '2':
-			return "이";
-		case '3':
-			return "삼";
-		case '4':
-			return "사";
-		case '5':
-			return "오";
-		case '6':
-			return "육";
-		case '7':
-			return "칠";
-		case '8':
-			return "팔";
-		case '9':
-			return "구";
+	static String[] num = {"영","일","이","삼","사","오","육","칠","팔","구"};
+	static String[] lowOrder = {"","십","백","천"};
+	static String[] bigOrder = {"원","만","억","조"};
 
-		}
-		return "";
+	public static String readNumber(char ch) {
+		
+		String str = "";
+		int i = ch - '0';
+		str = num[i];
+		
+		return str;
 	}
-
-	// ,와 원 제외한 문자열 만들기
-	public static void numberFilter(String example) {
-
-		ArrayList<Character> arr = new ArrayList<Character>();
-		Stack<String> result = new Stack<String>();
-		int len = example.length();
-
-		for (int i = len - 1; i >= 0; i--) {
-			char ch = example.charAt(i);
+	
+	public static String reString(String str) {
+		
+		int len = str.length();
+		String result = "";
+		
+		for (int i = len-1 ; i >= 0 ; i--) {
+			char ch = str.charAt(i);
 			if (ch == ',' || ch == '원') {
 				continue;
 			} else {
-				arr.add(ch);
+				String reStr = readNumber(ch);
+				result += reStr;
+			}
+		}
+		
+		return result;
+	}
+	
+	public static void solution(String str) {
+		
+		String answer = "";
+		String reStr = reString(str);
+		
+		for(int i = 0 ; i < reStr.length() ; i++) {
+			String tmp = "";
+			if(i%4 == 3) {
+				if(reStr.charAt(i) != '영') {
+					tmp += reStr.charAt(i);	
+				}
+				tmp += lowOrder[3];
+				System.out.println(tmp);
+				answer += tmp;
+			} else if (i%4 == 2) {
+				if(reStr.charAt(i) != '영') {
+					tmp += reStr.charAt(i);
+				}
+				tmp += lowOrder[2];	
+				System.out.println(tmp);
+				answer += tmp;
+			} else if (i%4 == 1) {
+				if(reStr.charAt(i) != '영') {
+					tmp += reStr.charAt(i);	
+				}
+				tmp += lowOrder[1];
+				System.out.println(tmp);
+				answer += tmp;
+			} else if (i%4 == 0) {
+				if(reStr.charAt(i) != '영') {
+					tmp += reStr.charAt(i);	
+				}
+				tmp += lowOrder[0];
+				if(reStr.charAt(i) != '영') {
+					tmp += bigOrder[i/4];	
+					System.out.println(tmp);
+				}
+				answer += tmp;
 			}
 		}
 
-		result = unitNumber(arr);
-
-		while (!result.isEmpty()) {
-			System.out.println("result" + result.pop());
-		}
-
+		
 	}
 
-	// 천 이상 숫자 단위
-	public static Stack<String> unitNumber(ArrayList<Character> arr) {
-
-		int order = 0;
-		Stack<String> stack = new Stack<String>();
-
-		for (int j = 0; j < arr.size(); j++) {
-
-			String str = "";
-			char arrNumber = arr.get(j);
-
-			if (j % 4 == 1) {
-				str = str + readNumber(arrNumber, order) + '십';
-				stack.push(str);
-				System.out.println("str" + str);
-			} else if (j % 4 == 2) {
-				str = str + readNumber(arrNumber, order) + '백';
-				stack.push(str);
-				System.out.println("str" + str);
-			} else if (j % 4 == 3) {
-				str = str + readNumber(arrNumber, order) + '천';
-				stack.push(str);
-				System.out.println("str" + str);
-			} else if (j % 4 == 0) {
-				str = str + readNumber(arrNumber, order) + orderNumber(order);
-				str.replace(" ", "");
-				stack.push(str);
-				System.out.println("str" + str);
-				order++;
-			}
-		}
-
-		return stack;
-	}
-
-	// 만 이상 숫자 단위
-	public static String orderNumber(int it) {
-		String order = "";
-		switch (it) {
-		case 0:
-			order = "원";
-			break;
-		case 1:
-			//만 단위 이후 띄어쓰기
-			order = "만 ";
-			break;
-		case 2:
-			order = "억";
-			break;
-		case 3:
-			order = "조";
-			break;
-		}
-		return order;
-	}
-
+	
 	public static void main(String[] args) {
-
-		String example = "1,204,567,890원";
-
-		numberFilter(example);
+		
+		String ex;
+		Scanner sc = new Scanner(System.in);
+		ex = sc.nextLine();
+		solution(ex);
 
 	}
-
 }
